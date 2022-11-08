1b317256c55f7db99f29888cf513ad909987c1c3
Sab01123 <104069112+Sab01123@users.noreply.github.com>
2022-11-09 02:32:36 +0530

    added

---
+++ onlinecabbookingapp/src/test/java/com/cabBooking/OnlinecabbookingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinecabbookingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
