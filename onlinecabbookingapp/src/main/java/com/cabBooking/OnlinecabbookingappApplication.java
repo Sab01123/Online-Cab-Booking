15fd488dd5c207f07fd6d0742e38dc644376d9e3
Sab01123 <104069112+Sab01123@users.noreply.github.com>
2022-11-09 02:28:06 +0530

    day_1

---
+++ onlinecabbookingapp/src/main/java/com/cabBooking/OnlinecabbookingappApplication.java
@@ -0,0 +1,13 @@
+package com.cabBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinecabbookingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinecabbookingappApplication.class, args);
+	}
+
+}
