c90a3598fe210bb92ee9b82fdd30aedac1c50794
= <=>
2022-11-09 22:07:51 +0530

    first-commit

---
+++ onlinecabbookingapp/src/main/java/com/cabBooking/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.cabBooking.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cabBooking.models.Customer;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+	
+}
