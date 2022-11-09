934a54b5034ccd2b61942af1c88551da796bb01e
Namdev Patil <namdevpatilm@gmail.com>
2022-11-09 23:54:13 +0530

    last added

---
+++ onlinecabbookingapp/src/main/java/com/cabBooking/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.cabBooking.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class LoginController {
+
+	
+
+}
--- onlinecabbookingapp/src/main/java/com/cabBooking/models/Admin.java
+++ onlinecabbookingapp/src/main/java/com/cabBooking/models/Admin.java
@@ -1,5 +1,19 @@
 package com.cabBooking.models;
 
-public class Admin {
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Entity
+@Data
+@NoArgsConstructor
+@PrimaryKeyJoinColumn(name="adminId")
+public class Admin extends Abstractuser {
+
+	
+	
 }
