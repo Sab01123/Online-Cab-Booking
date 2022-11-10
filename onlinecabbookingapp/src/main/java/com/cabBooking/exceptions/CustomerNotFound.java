f5872a3e3a8d7ca8b64f87ef5ed858a679ecedf4
= <=>
2022-11-10 11:05:59 +0530

    exceptions

---
+++ onlinecabbookingapp/src/main/java/com/cabBooking/exceptions/CustomerNotFound.java
@@ -0,0 +1,14 @@
+package com.cabBooking.exceptions;
+
+public class CustomerNotFound extends Exception{
+
+	public CustomerNotFound() {
+	
+	}
+	
+	public CustomerNotFound(String message) {
+		super(message);
+	}
+	
+	
+}
