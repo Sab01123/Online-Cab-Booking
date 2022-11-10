1f6bc190021eb97d075c9617ccfcd000c03e9b80
Sab01123 <104069112+Sab01123@users.noreply.github.com>
2022-11-10 21:06:18 +0530

    CabSide Done

--- onlinecabbookingapp/src/main/java/com/cabBooking/models/Cab.java
+++ onlinecabbookingapp/src/main/java/com/cabBooking/models/Cab.java
@@ -6,6 +6,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,6 +23,7 @@ public class Cab {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer cabId;
+	@NotEmpty(message = "Cab type can't be Empty..")
 	private String cabType;
 	private Float perKmRate;
 
